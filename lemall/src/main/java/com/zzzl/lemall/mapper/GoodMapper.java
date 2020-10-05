7ecb9704d0957b39e9bab3828769cba58e8bf8b2
xiaoQuanTxue <673038225@qq.com>
2020-10-05 15:43:55 +0800

    增加good，goodexample

---
+++ lemall/src/main/java/com/zzzl/lemall/mapper/GoodMapper.java
@@ -0,0 +1,30 @@
+package com.zzzl.lemall.mapper;
+
+import com.zzzl.lemall.domain.Good;
+import com.zzzl.lemall.domain.GoodExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface GoodMapper {
+    long countByExample(GoodExample example);
+
+    int deleteByExample(GoodExample example);
+
+    int deleteByPrimaryKey(Integer goodId);
+
+    int insert(Good record);
+
+    int insertSelective(Good record);
+
+    List<Good> selectByExample(GoodExample example);
+
+    Good selectByPrimaryKey(Integer goodId);
+
+    int updateByExampleSelective(@Param("record") Good record, @Param("example") GoodExample example);
+
+    int updateByExample(@Param("record") Good record, @Param("example") GoodExample example);
+
+    int updateByPrimaryKeySelective(Good record);
+
+    int updateByPrimaryKey(Good record);
+}
